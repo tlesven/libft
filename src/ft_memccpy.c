ac3df37eab9b9f7e6157b26efce2a7889bc75392
Trouz <timo.lesven@gmail.com>
2018-11-19 14:32:46 +0100

    memccpy correction

--- src/ft_memccpy.c
+++ src/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: tlesven <tlesven@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 13:48:13 by tlesven           #+#    #+#             */
-/*   Updated: 2013/12/08 20:28:48 by tlesven          ###   ########.fr       */
+/*   Updated: 2018/11/19 13:37:55 by tlesven          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 void	*ft_memccpy(void *s1, const void *s2, int c, size_t n)
 {
-	unsigned int	i;
-	char			*sub1;
-	const char		*sub2;
+	unsigned int		i;
+	unsigned char		*sub1;
+	const unsigned char	*sub2;
+	unsigned char		uc;
 
+	i = 0;
 	sub1 = s1;
 	sub2 = s2;
-	i = 0;
+	uc = c;
 	if (n == 0)
 		return (NULL);
 	while (i < n)
 	{
 		sub1[i] = sub2[i];
-		if (sub2[i] == c)
+		if (sub2[i] == uc)
 			return (s1 + i + 1);
 		i++;
 	}
