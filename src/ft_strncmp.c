f64da7456f40c078950225407351a1b64cb6d76d
Trouz <timo.lesven@gmail.com>
2018-11-19 22:21:09 +0100

    correction strncmp

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tlesven <tlesven@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/20 15:27:00 by tlesven           #+#    #+#             */
-/*   Updated: 2018/11/19 16:15:06 by tlesven          ###   ########.fr       */
+/*   Updated: 2018/11/19 22:19:07 by tlesven          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,9 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] == '\200' && s2[i] == '\0')
-			return (s2[i] - s1[i]);
-		if (s1[i] != s2[i])
-			break ;
-		if (s1[i] == '\0' || s2[i] == '\0')
-			break ;
+		if (s1[i] == '\0' || s2[i] == '\0' || s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }
