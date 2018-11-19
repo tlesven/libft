f6dc217b12cea028ea166d77244752b5b19c46b1
Trouz <timo.lesven@gmail.com>
2018-11-19 19:12:01 +0100

    correction strmap & strampi

--- src/ft_strmap.c
+++ src/ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: tlesven <tlesven@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/26 12:31:21 by tlesven           #+#    #+#             */
-/*   Updated: 2015/05/13 13:51:03 by tlesven          ###   ########.fr       */
+/*   Updated: 2018/11/19 19:09:48 by tlesven          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ char	*ft_strmap(char const *s, char (*f)(char))
 	char	*str;
 	int		i;
 
-	str = (char *)ft_memalloc(sizeof(s) * ft_strlen(s));
+	if (!s || !f)
+		return (NULL);
+	str = (char *)ft_memalloc(ft_strlen(s) + 1);
+	if (str == NULL)
+		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
 	{
