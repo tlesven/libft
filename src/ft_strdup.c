d2f2a00b659231c611c82ecc712dc216d3ea2c3f
Trouz <timo.lesven@gmail.com>
2018-11-19 15:54:10 +0100

    correction strdup

--- src/ft_strdup.c
+++ src/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tlesven <tlesven@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 15:34:40 by tlesven           #+#    #+#             */
-/*   Updated: 2015/05/13 13:50:02 by tlesven          ###   ########.fr       */
+/*   Updated: 2018/11/19 15:48:54 by tlesven          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strdup(const char *s)
 	unsigned int	i;
 
 	scopy = (char*)ft_memalloc(sizeof(*scopy) * (ft_strlen(s) + 1));
+	if (!scopy)
+		return (NULL);
 	i = 0;
 	while (i < ft_strlen(s))
 	{
