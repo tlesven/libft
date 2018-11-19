53cda806fdc5b166dd34597aedd75e2edfbd23ef
Trouz <timo.lesven@gmail.com>
2018-11-19 18:35:35 +0100

    atoi correction

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,15 +6,16 @@
 /*   By: tlesven <tlesven@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/16 13:00:24 by tlesven           #+#    #+#             */
-/*   Updated: 2015/03/31 13:48:22 by tlesven          ###   ########.fr       */
+/*   Updated: 2018/11/19 18:32:54 by tlesven          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "stdio.h"
 
 static int	ft_nbr(int neg, char *str)
 {
-	int		result;
+	long long	result;
 	int		i;
 
 	result = 0;
@@ -28,7 +29,7 @@ static int	ft_nbr(int neg, char *str)
 		result = (str[i] - '0') + result;
 		i++;
 	}
-	return (neg * result);
+	return ((int)(neg * result));
 }
 
 int			ft_atoi(char *str)
@@ -36,16 +37,16 @@ int			ft_atoi(char *str)
 	int		i;
 
 	i = 0;
-	while (str[i] != '\0' && str[i] != '\200')
+	while (str[i] != '\0' && str[i] != '\200' && str[i] != '\e')
 	{
+		if (ft_isalpha(str[i]))
+			return (0);
 		if ((str[i] <= 13 && str[i] >= 9) || str[i] == 32)
 			i++;
 		if (str[i] == '-')
 			return (ft_nbr(-1, str + i));
 		if (str[i] == '+' || ft_isdigit(str[i]) == 1)
 			return (ft_nbr(1, str + i));
-		if (ft_isalpha(str[i]))
-			return (0);
 		i++;
 	}
 	return (0);
