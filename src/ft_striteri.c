8335b97fec9ca2663622a08adadb6afe53235a2b
Trouz <timo.lesven@gmail.com>
2018-11-19 18:59:06 +0100

    correction ft_striteri

--- src/ft_striteri.c
+++ src/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: tlesven <tlesven@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/26 12:01:06 by tlesven           #+#    #+#             */
-/*   Updated: 2013/11/26 13:52:30 by tlesven          ###   ########.fr       */
+/*   Updated: 2018/11/19 18:58:21 by tlesven          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	int		i;
 
 	i = 0;
-	while (s[i] != '\0')
+	if(s && f)
 	{
-		(*f)(i, &s[i]);
-		i++;
+		while (s[i] != '\0')
+		{
+			(*f)(i, &s[i]);
+			i++;
+		}
 	}
 }
